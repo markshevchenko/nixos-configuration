d5845c56b64fa34c1da6f6d39c6fead464d386e3
Mark Shevchenko <markshevchenko@gmail.com>
2023-03-20 06:57:35 +0300

    Setup sound

--- configuration.nix
+++ configuration.nix
@@ -66,15 +66,15 @@
 
   # Enable sound with pipewire.
   sound.enable = true;
-  hardware.pulseaudio.enable = false;
+  hardware.pulseaudio.enable = true;
   security.rtkit.enable = true;
   services.pipewire = {
-    enable = true;
+    enable = false;
     alsa.enable = true;
     alsa.support32Bit = true;
     pulse.enable = true;
     # If you want to use JACK applications, uncomment this
-    #jack.enable = true;
+    jack.enable = true;
 
     # use the example session manager (no others are packaged yet so this is enabled by default,
     # no need to redefine it in your config for now)
@@ -109,15 +109,18 @@
   #  wget
     git
     google-chrome
+    dotnet-sdk_7
+    rustc
+    cargo
+    rust-analyzer
     emacs
     vscode
     jetbrains.rider
     tdesktop
     whatsapp-for-linux
-    dotnet-sdk_7
-    cargo
     docker
     ntfs3g
+    pavucontrol
     audacity
     (writeShellScriptBin "startvpn.sh" ''
       sudo systemctl start openvpn-aezaVPN.service
@@ -141,7 +144,10 @@
   # services.openssh.enable = true;
 
   services.openvpn.servers = {
-    aezaVPN = { config = '' config /root/nixos/openvpn/aeza.conf ''; };
+    aezaVPN = {
+      autoStart = false;
+      config = '' config /root/nixos/openvpn/aeza.conf '';
+    };
   };
 
   # Open ports in the firewall.
